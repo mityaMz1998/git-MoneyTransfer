cb931f4cbc74ed4a9a87af7c0bf9894bee0e5c94
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-01-13 20:36:59 +0300

    Fixed change

--- WpfMoneyTransfer/Views/MainWindow.xaml.cs
+++ WpfMoneyTransfer/Views/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Input;
 namespace WpfMoneyTransfer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for the form MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
